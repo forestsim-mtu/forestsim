2139177ca919150a56364f90f58dcad365da8792
rjzupkoii <rjzii@yahoo.com>
2017-01-23 15:54:16 -0500

    Remove Houghton model specific code from the master branch.

--- src/edu/mtu/vip/houghton/species/WesternUPSpecies.java
+++
@@ -1,9 +0,0 @@
-package edu.mtu.vip.houghton.species;
-
-import edu.mtu.environment.Species;
-
-public interface WesternUPSpecies extends Species {
-	double getDbhGrowth();
-	double getMaximumDbh();
-	String getDataFile();
-}
