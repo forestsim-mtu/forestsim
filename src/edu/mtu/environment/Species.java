43286bdd794e35bf107b19207421d75f1f6fb14d
rjzupkoii <rjzii@yahoo.com>
2017-06-04 13:19:14 -0400

    Corrected documentation, added example code.

--- src/edu/mtu/environment/Species.java
+++ src/edu/mtu/environment/Species.java
@@ -22,12 +22,26 @@ public interface Species {
 	double getStemWoodBiomassRatio(double dbh);
 			
 	/**
+	 * Get the annual mean DBH growth of the species.
+	 * 
+	 * @return The annual mean DBH growth, in cm.
+	 */
+	double getDbhGrowth();
+	
+	/**
 	 * Get the height of a representative tree given the DBH.
 	 * 
 	 * @param dbh The DBH of the given tree, in cm.
 	 * @return Approximate height of the given tree, in m.
 	 */
 	double getHeight(double dbh);
+	
+	/**
+	 * Get the maximum DBH of the species.
+	 * 
+	 * @return The maximum DBH, in cm.
+	 */
+	double getMaximumDbh(); 
 	
 	/**
 	 * Get the name of the species.
